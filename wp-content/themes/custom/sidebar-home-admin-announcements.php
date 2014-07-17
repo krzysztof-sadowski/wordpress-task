106797b486de61fb58ebe4293402dfd66f4dff6b
Krzysztof Sadowski <krzysztof.sadowski@polcode.net>
2014-07-17 12:24:50 +0200

    Ready for revision

--- wp-content/themes/custom/sidebar-home-admin-announcements.php
+++
@@ -1,5 +0,0 @@
-<div class="row" id="sidebar-home-admin-announcements-row">
-    <div class="col-xs-12" id="sidebar-home-admin-announcements-col">
-        <?php dynamic_sidebar( 'home-admin-announcements' ); ?>
-    </div>
-</div>
---
+++ wp-content/themes/custom/sidebar-user-profile-top.php
@@ -0,0 +1,5 @@
+<div class="row" id="sidebar-home-top-row">
+    <div class="col-xs-12" id="sidebar-home-top-col">
+        <?php dynamic_sidebar( 'home-top' ); ?>
+    </div>
+</div>
